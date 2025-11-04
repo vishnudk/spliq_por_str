bed058b16a61dbab7136d6edba6fc9c79fa43eae
vishnudk <vishnudk179@gmail.com>
2025-11-04 21:09:20 +0530

    added pwa capability.

--- SRC/UI/AppUI/src/app/base-comp/base-comp.ts
+++ SRC/UI/AppUI/src/app/base-comp/base-comp.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import {MatCardModule} from '@angular/material/card';
 
 @Component({
   selector: 'app-base-comp',
-  imports: [],
+  imports: [MatSlideToggleModule, MatCardModule],
   templateUrl: './base-comp.html',
   styleUrl: './base-comp.css',
 })
