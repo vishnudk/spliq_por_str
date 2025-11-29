89eb79f065605c74044163b1876463d1be4ccedb
vishnudk <vishnudk179@gmail.com>
2025-11-29 13:31:05 +0530

    Added apm for expense, group, and user mgr

--- SRC/BL/group_mgr/group_mgr/settings.py
+++ SRC/BL/group_mgr/group_mgr/settings.py
@@ -37,10 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+      'elasticapm.contrib.django'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+     'elasticapm.contrib.django.middleware.TracingMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -50,7 +52,14 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'group_mgr.urls'
-
+ELASTIC_APM = {
+    'SERVICE_NAME': 'group_mgr_service',
+    'SECRET_TOKEN': '',   # optional in local
+    'SERVER_URL': 'http://apm-server:8200',   # where APM server runs
+    'DISABLE_SEND' : False,
+    'ENVIRONMENT': 'development',            # dev/stage/prod
+     "TRANSPORT_CLASS": "elasticapm.transport.http.AsyncTransport",
+}
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
